4755db869f4e458f15654a140e144d88bee7932c
AntonioPerez25 <l.perez@ibsolutionsgroup.com>
2024-10-20 11:03:52 -0600

    Esquema terminado

--- Model/schema.sql
+++ Model/schema.sql
@@ -14,9 +14,6 @@ create table cargos (
 
 CREATE TABLE configuracion (
     id_config INT AUTO_INCREMENT PRIMARY KEY,
-    id_usuario INT NOT NULL,
     horas_laborales INT NOT NULL,
-    costo_manufactura DECIMAL(10, 2) NOT NULL,
-    salario DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE
+    costo_manufactura DECIMAL(10, 2) NOT NULL
 );
