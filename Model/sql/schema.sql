dddf9d15d8ecc9168205786f7c5ca0437b9754bc
AntonioPerez25 <l.perez@ibsolutionsgroup.com>
2024-11-01 10:54:00 -0600

    Esquema de la base de datos actualizado

--- Model/sql/schema.sql
+++ Model/sql/schema.sql
@@ -2,7 +2,14 @@ create table usuarios (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR (255) NOT NULL UNIQUE,
     contrasena VARCHAR (255) NOT NULL,
-    rol VARCHAR (50) NOT NULL
+    rol VARCHAR (50) NOT NULL,
+    id_area INT NOT NULL,
+    FOREIGN KEY (id_area) REFERENCES area(id_area)
+);
+
+create table areas (
+    id_area INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_area VARCHAR (100) NOT NULL
 );
 
 create table cargos (
@@ -10,15 +17,19 @@ create table cargos (
     cargo VARCHAR (100) NOT NULL,
     salario DECIMAL (10,2) NOT NULL,
     margen_utilidad DECIMAL (5,2) NOT NULL,
+    precio_iva_cap DECIMAL (10,2),
     precio_iva_esc DECIMAL (10,2),
     precio_iva_cli DECIMAL (10,2),
-    precio_iva_cap DECIMAL (10,2)
+    id_area INT NOT NULL,
+    FOREIGN KEY (id_area) REFERENCES areas(id_area)
 );
 
 CREATE TABLE configuracion (
     id_config INT AUTO_INCREMENT PRIMARY KEY,
     horas_laborales INT NOT NULL,
-    costo_manufactura DECIMAL(10, 2) NOT NULL
+    costo_manufactura DECIMAL(10, 2) NOT NULL,
+    id_usuario INT NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
 );
 
 USUARIO: coti_ibs
